ff0c1cc9cf15c2a4493486b6f3482929a9087d43
aevitas <m4rtijn@gmail.com>
2021-04-28 17:08:15 +0200

    Rename GetEpoch

--- SnowflakeId/SnowflakeId/MonotonicTimer.cs
+++ SnowflakeId/SnowflakeId/MonotonicTimer.cs
@@ -5,12 +5,12 @@ namespace SnowflakeId
 {
     internal static class MonotonicTimer
     {
-        private static readonly long s_epoch = GetEpoch();
+        private static readonly long s_epoch = GetInstanceEpoch();
         private static readonly Stopwatch s_stopwatch = Stopwatch.StartNew();
 
         public static long ElapsedMilliseconds => s_epoch + s_stopwatch.ElapsedMilliseconds;
 
-        private static long GetEpoch()
+        private static long GetInstanceEpoch()
         {
             DateTimeOffset epoch = new DateTimeOffset(2015, 1, 1, 0, 0, 0, 0, TimeSpan.Zero);
             TimeSpan deltaNow = DateTimeOffset.UtcNow - epoch;
