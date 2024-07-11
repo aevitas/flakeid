2f57bd71dcf2841c2297d74525d0ab61cb0a6284
Szakáts Alpár Zsolt <szakats.alpar@bartex.hu>
2024-07-11 16:16:56 +0200

    Update tests to support .NET Standard 2.0
    Small changes in the code to build on lower (7.3) C# versions

--- src/FlakeId.Tests/IdExtensionsTests.cs
+++ src/FlakeId.Tests/IdExtensionsTests.cs
@@ -2,47 +2,48 @@
 using FlakeId.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace FlakeId.Tests;
-
-[TestClass]
-public class IdExtensionsTests
+namespace FlakeId.Tests
 {
-    [TestMethod]
-    public void Id_ToDateTimeOffset()
+    [TestClass]
+    public class IdExtensionsTests
     {
-        Id id = Id.Create();
-        DateTimeOffset timeStamp = id.ToDateTimeOffset();
-        DateTimeOffset now = DateTimeOffset.Now;
-        TimeSpan delta = now - timeStamp;
+        [TestMethod]
+        public void Id_ToDateTimeOffset()
+        {
+            Id id = Id.Create();
+            DateTimeOffset timeStamp = id.ToDateTimeOffset();
+            DateTimeOffset now = DateTimeOffset.Now;
+            TimeSpan delta = now - timeStamp;
 
-        Assert.IsTrue(delta.Seconds <= 1);
-    }
+            Assert.IsTrue(delta.Seconds <= 1);
+        }
 
-    [TestMethod]
-    public void Id_ToUnixTimeMilliseconds()
-    {
-        Id id = Id.Create();
-        long timestamp = id.ToUnixTimeMilliseconds();
-        long now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        [TestMethod]
+        public void Id_ToUnixTimeMilliseconds()
+        {
+            Id id = Id.Create();
+            long timestamp = id.ToUnixTimeMilliseconds();
+            long now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 
-        Assert.IsTrue(now - timestamp < 100);
-    }
+            Assert.IsTrue(now - timestamp < 100);
+        }
 
-    [TestMethod]
-    public void Id_IsValid()
-    {
-        Id id = Id.Create();
-        bool isValid = id.IsSnowflake();
+        [TestMethod]
+        public void Id_IsValid()
+        {
+            Id id = Id.Create();
+            bool isValid = id.IsSnowflake();
 
-        Assert.IsTrue(isValid);
-    }
+            Assert.IsTrue(isValid);
+        }
 
-    [TestMethod]
-    public void Id_ToStringIdentifier_ProducesValidId()
-    {
-        Id id = Id.Create();
-        string s = id.ToStringIdentifier();
+        [TestMethod]
+        public void Id_ToStringIdentifier_ProducesValidId()
+        {
+            Id id = Id.Create();
+            string s = id.ToStringIdentifier();
 
-        Assert.AreNotEqual(default, s);
+            Assert.AreNotEqual(default, s);
+        }
     }
 }
